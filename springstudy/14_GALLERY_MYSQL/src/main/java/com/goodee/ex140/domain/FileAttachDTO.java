575faf52a780309c03066f9176674586a957c13a
gdj45 <wltlgus110@naver.com>
2022-07-07 18:17:58 +0900

    2022-07-07

---
+++ springstudy/14_GALLERY_MYSQL/src/main/java/com/goodee/ex140/domain/FileAttachDTO.java
@@ -0,0 +1,22 @@
+package com.goodee.ex140.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class FileAttachDTO {
+
+	private Long fileAttachNo;
+	private String path;
+	private String origin;
+	private String saved;
+	private Long galleryNo;
+	private Integer downloadCnt;
+	
+}
